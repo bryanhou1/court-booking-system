799325786b0966dd5414d170a6544df61daee587
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-04-30 16:00:42 -0400

    filled in migration files

--- db/migrate/20170430195354_create_bookings.rb
+++ db/migrate/20170430195354_create_bookings.rb
@@ -1,4 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
+  	create_table :bookings do |t|
+  		t.string :time
+  		t.integer :user_id
+  		t.integer :court
+
+  		t.timestamps
+  	end
   end
 end

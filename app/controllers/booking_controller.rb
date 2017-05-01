8fa4430ca06c6e2f6dda51638973b8f35719beec
bryanhou1 <Hou@wpa159072.wireless.mcgill.ca>
2017-05-01 14:43:00 -0400

    buildinging booking controller

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,14 +1,13 @@
 class BookingController < ApplicationController
 
 	get '/bookings/new' do
-		i
 		erb :'/booking/new'
 	end
 
 	post '/bookings' do
 		booking = Booking.new(params)
 
-		if @booking.save
+		if booking.save
 			redirect "/bookings/#{booking.id}"
 		else
 			#flash msg
@@ -20,15 +19,32 @@ class BookingController < ApplicationController
 		@booking = Booking.find(params[:id])
 		if @booking
 			erb :'/bookings/show'
+		end
 	end
 
-	get '/bookings/edit' do
+	get '/bookings/:id/edit' do
+		@booking = Booking.find_by(id: params[:id])
+		if @booking
+			erb :'/bookings/edit'
+		else
+			"error" #fix
+		end
 	end
 
-	post '/bookings/:id' do
+	patch '/bookings/:id' do
+		booking = Booking.find(params[:id])
 
+  	if booking.save
+  	redirect "/bookings/#{params[:id]}"
+  	else
+  		#fix
+  	end
 	end
 
-	delete '/bookings' do
-	end
+	delete '/bookings/:id/delete' do
+  	booking = Booking.find(params[:id])
+  	booking.destroy
+  	redirect '/bookings'
+  end
+
 end

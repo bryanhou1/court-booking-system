c57bcaf434c43b0129d85b03dc473b3381ceb699
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-05-14 16:16:26 -0400

    altered seeds for better demonstration and modified /bookings/show to demonstrate today's booking as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,89 +3,62 @@ User.create(username: "user_2", password: "22")
 User.create(username: "user_3", password: "33")
 
 require 'date'
+time = DateTime.new(2017, 5, 1, 0, 0, 0, '-04:00')
 Booking.create([
 {
-	time: DateTime.new(2017, 5, 1, 0, 0, 0, '-04:00'),
+	time: time,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 2, 0, 0, 0, '-04:00'),
+	time: time + 1.day,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 3, 0, 0, 0, '-04:00'),
+	time: time + 9.days,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 11, 0, 0, 0, '-04:00'),
+	time: time + 10.days,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 12, 0, 0, 0, '-04:00'),
+	time: time + 11.days,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 13, 0, 0, 0, '-04:00'),
+	time: time + 12.days,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 14, 0, 0, 0, '-04:00'),
-	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
-},{
-	time: DateTime.new(2017, 5, 14, 0, 0, 0, '-04:00'),
+	time: time + 13.days,
 	user_id: 2,
-	court: 2,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 2
 },{
-	time: DateTime.new(2017, 5, 15, 0, 0, 0, '-04:00'),
+	time: time + 14.days,
 	user_id: 2,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 16, 0, 0, 0, '-04:00'),
+	time: time + 15.days,
 	user_id: 1,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 17, 0, 0, 0, '-04:00'),
+	time: time + 16.days,
 	user_id: 3,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 18, 0, 0, 0, '-04:00'),
+	time: time + 17.days,
 	user_id: 2,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 19, 0, 0, 0, '-04:00'),
+	time: time + 18.days,
 	user_id: 2,
-	court: 1,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 1
 },{
-	time: DateTime.new(2017, 5, 17, 0, 0, 0, '-04:00'),
+	time: time + 16.days,
 	user_id: 2,
-	court: 2,
-	created_at: DateTime.now,
-	updated_at: DateTime.now
+	court: 2
+},{
+	time: time + 17.days,
+	user_id: 3,
+	court: 2
 }])
